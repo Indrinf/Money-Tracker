9a089b099194706e983182f45be1112cab8c1d42
Indri Nurfiani <indrinurfiani99@gmail.com>
2022-12-14 22:02:54 +0700

    create transaction page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:money_tracking/pages/category_page.dart';
 import 'package:money_tracking/pages/home_page.dart';
+import 'package:money_tracking/pages/transaction_page.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -49,7 +50,15 @@ class _MainPageState extends State<MainPage> {
       floatingActionButton: Visibility(
         visible: (currentIndex == 0) ? true : false,
         child: FloatingActionButton(
-          onPressed: () {},
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(
+              builder: (context) => TransactionPage(),
+            ))
+                .then((value) {
+              setState(() {});
+            });
+          },
           backgroundColor: Colors.teal,
           child: Icon(Icons.add),
         ),
