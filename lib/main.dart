be7a6b80da7af98cc9b3b57f71a47e037597ecb9
syhrlanwr <syhrlanwr@gmail.com>
2022-12-30 10:30:37 +0700

    fix main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:money_tracking/pages/main_page.dart';
+import 'package:money_tracking/pages/add_balance_page.dart';
+import 'package:money_tracking/pages/add_transaction_page.dart';
+import 'package:money_tracking/pages/home_page.dart';
+import 'package:money_tracking/pages/login_page.dart';
+import 'package:money_tracking/pages/signup_page.dart';
+import 'package:money_tracking/pages/splash.dart';
+import 'package:money_tracking/pages/transaction_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,8 +17,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-      theme: ThemeData(primarySwatch: Colors.teal),
-    );
+        title: 'MoneyTracker',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        routes: {
+          '/': (_) => const Splash(),
+          '/login': (_) => const Login(),
+          '/signup': (_) => const Signup(),
+          '/home': (_) => const Home(),
+          '/add-transaction': (_) => const AddTransaction(),
+          '/transaction': (_) => const Transaction(),
+          '/add-balance': (_) => const AddBalance(),
+        },
+        initialRoute: '/');
   }
 }
