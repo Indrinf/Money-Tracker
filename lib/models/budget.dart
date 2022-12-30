0e9203ba2a1590ea1bf07fbe52be6b9f0c0ca1f8
syhrlanwr <syhrlanwr@gmail.com>
2022-12-30 10:32:18 +0700

    add models

---
+++ lib/models/budget.dart
@@ -0,0 +1,15 @@
+class Budget {
+  final int totalBudget;
+
+  Budget({required this.totalBudget});
+
+  factory Budget.fromJson(Map<String, dynamic> json) {
+    return Budget(
+      totalBudget: json['total_budget'],
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'total_budget': totalBudget,
+      };
+}
